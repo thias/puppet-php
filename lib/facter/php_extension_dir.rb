8b1de2bfbf6f3dfde86189ff9d91c3491f91a819
David de Boer <david@ddeboer.nl>
2013-07-04 15:06:46 +0200

    Differentiate extension dir based on architecture

--- lib/facter/php_extension_dir.rb
+++ lib/facter/php_extension_dir.rb
@@ -2,6 +2,14 @@
 # to be installed.
 Facter.add("php_extension_dir") do
   setcode do
-    Facter::Util::Resolution.exec('php -r "echo ini_get(\'extension_dir\');"')
+    if File.exist? "/usr/bin/php"
+      Facter::Util::Resolution.exec('php -r "echo ini_get(\'extension_dir\');"')
+    else
+      if Facter.value("architecture") == "x86_64"
+        "/usr/lib64/php/modules"
+      else
+        "/usr/lib/php/modules"
+      end
+    end
   end
 end
