4ab7a953c85952f9a09710f6e79846918a8d0581
Seb Dangerfield <seb.dangerfield@wearetwogether.com>
2013-07-16 14:43:42 +0100

    Updated php fpm params

--- manifests/params.pp
+++ manifests/params.pp
@@ -21,13 +21,13 @@ class php::params {
       $cli_package_name = 'php-cli'
       $fpm_package_name = 'php-fpm'
       $fpm_service_name = 'php-fpm'
-      $fpm_pool_dir = '/etc/php/fpm/pool.d'
-      $fpm_conf_dir = '/etc/php/fpm'
-      $fpm_error_log = '/var/log/php-fpm.log'
+      $fpm_pool_dir = '/etc/php-fpm.d'
+      $fpm_conf_dir = '/etc'
+      $fpm_error_log = '/var/log/php-fpm/error.log'
       $fpm_pid = '/var/run/php-fpm.pid'
-      $httpd_package_name = 'apache2'
-      $httpd_service_name = 'apache2'
-      $httpd_conf_dir = '/etc/apache2/conf.d'
+      $httpd_package_name = 'httpd'
+      $httpd_service_name = 'httpd'
+      $httpd_conf_dir = '/etc/httpd/conf.d'
     }
 
     default: {
