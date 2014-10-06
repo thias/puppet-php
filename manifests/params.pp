f8b5610e6fdae2a5e7c2f8dd60c966796ead2dbd
geek4unix <geek4unix@gmail.com>
2014-10-06 15:29:39 +0100

    added changes for php55, part 2

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,9 +2,9 @@ class php::params {
   case $::osfamily {
     'Debian': {
       $php_package_name = 'php5'
-      $php_apc_package_name = 'php5-php-apc'
-      $common_package_name = 'php55-php-common'
-      $cli_package_name = 'php55-cli'
+      $php_apc_package_name = 'php-apc'
+      $common_package_name = 'php5-common'
+      $cli_package_name = 'php5-cli'
       $php_conf_dir = '/etc/php5/conf.d'
       $fpm_package_name = 'php5-fpm'
       $fpm_service_name = 'php5-fpm'
@@ -19,9 +19,9 @@ class php::params {
 
     default: {
       $php_package_name = 'php55'
-      $php_apc_package_name = 'php-pecl-apc'
-      $common_package_name = 'php-common'
-      $cli_package_name = 'php-cli'
+      $php_apc_package_name = 'php55-pecl-apc'
+      $common_package_name = 'php55-common'
+      $cli_package_name = 'php55-cli'
       $php_conf_dir = '/etc/php.d'
       $fpm_package_name = 'php-fpm'
       $fpm_service_name = 'php-fpm'
