12f1e8f7a0d940af72af85dd55a43747310fc548
Greg Lund-Chaix <greg@tag1consulting.com>
2016-09-01 15:46:46 -0700

    Initial systemd socket config
    * Creates php::fpm::systemd-socket-conf for systemd sockets
    * Adds systemd socket and service
    * Modified config file templates that allow for multiple php-fpm master
    processes.

---
+++ manifests/fpm/systemd-socket-conf.pp
@@ -0,0 +1,127 @@
+# Define: php::fpm::systemd-socket-conf
+#
+# PHP FPM pool configuration definition. Note that the original php-fpm package
+# includes a pre-configured one called 'www' so you should either use that name
+# in order to override it, or "ensure => absent" it.
+#
+# Sample Usage:
+#  php::fpm::systemd-socket-conf { 'www': ensure => absent }
+#  php::fpm::systemd-socket-conf { 'customer1':
+#      listen      => '/var/run/php-fpm/customer1.socket',
+#      listen_port => 9001,
+#      user        => 'customer1',
+#  }
+#  php::fpm::systemd-socket-conf { 'customer2':
+#      listen      => '/var/run/php-fpm/customer1.socket',
+#      listen_port => 9002,
+#      user        => 'customer2',
+#  }
+#
+define php::fpm::systemd-socket-conf (
+  $ensure                    = 'present',
+  $package_name              = undef,
+  $service_name              = undef,
+  $user                      = 'apache',
+  $group                     = undef,
+  $listen                    = undef,
+  $listen_port               = '9000',
+  # Puppet does not allow dots in variable names
+  $listen_backlog            = '-1',
+  $listen_owner              = undef,
+  $listen_group              = undef,
+  $listen_mode               = undef,
+  $listen_allowed_clients    = '127.0.0.1',
+  $process_priority          = undef,
+  $pm                        = 'dynamic',
+  $pm_max_children           = '50',
+  $pm_start_servers          = '5',
+  $pm_min_spare_servers      = '5',
+  $pm_max_spare_servers      = '35',
+  $pm_process_idle_timeout   = undef,
+  $pm_max_requests           = '0',
+  $pm_status_path            = undef,
+  $ping_path                 = undef,
+  $ping_response             = 'pong',
+  $slowlog                   = "/var/log/php-fpm/${name}-slow.log",
+  $request_slowlog_timeout   = undef,
+  $request_terminate_timeout = undef,
+  $rlimit_files              = undef,
+  $rlimit_core               = undef,
+  $chroot                    = undef,
+  $chdir                     = undef,
+  $catch_workers_output      = 'no',
+  $security_limit_extensions = undef,
+  $env                       = [],
+  $env_value                 = {},
+  $php_value                 = {},
+  $php_flag                  = {},
+  $php_admin_value           = {},
+  $php_admin_flag            = {},
+  $php_directives            = [],
+  $error_log                 = true,
+) {
+
+  include '::php::params'
+
+  $pool = $title
+
+  # Create systemd socket
+  file { "/usr/lib/systemd/system/php-fpm-${pool}.socket":
+    ensure  => $ensure,
+    content => template('php/fpm/pool.socket.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package[$fpm_package_name],
+  }
+
+  service { "php-fpm-${pool}.socket":
+    ensure => running,
+    start  => 'systemctl start php-fpm-${pool}.socket',
+    stop   => 'systemctl stop php-fpm-${pool}.socket',
+    status => 'systemctl status php-fpm-${pool}.socket',
+  }
+
+  # Create per-pool service
+  $service_name = 'php-fpm-${pool}.service'
+  file { "/usr/lib/systemd/system/php-fpm-${pool}.service":
+    ensure  => $ensure,
+    content => template('php/fpm/pool.service.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package[$fpm_package_name],
+  }
+
+  service { "${service_name}":
+    ensure => running,
+    start  => 'systemctl start php-fpm-${pool}.service',
+    stop   => 'systemctl stop php-fpm-${pool}.service',
+    status => 'systemctl status php-fpm-${pool}.service',
+  }
+
+  # Hack-ish to default to user for group too
+  $group_final = $group ? { undef => $user, default => $group }
+
+  # This is much easier from classes which inherit params...
+  $fpm_package_name = $package_name ? {
+    undef   => $::php::params::fpm_package_name,
+    default => $package_name,
+  }
+  $fpm_service_name = $service_name ? {
+    undef   => $::php::params::fpm_service_name,
+    default => $service_name,
+  }
+
+  file { "${php::params::fpm_pool_dir}/${pool}.conf":
+    ensure  => $ensure,
+    content => template('php/fpm/pool-systemd.conf.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package[$fpm_package_name],
+    notify  => Service[$fpm_service_name],
+  }
+
+}
+
